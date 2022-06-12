eb405fa0cb8cee701e701e6ac6b7acea523540cc
Joshua Carrillo Gonzalez <joshuacarrillogonzalez@gmail.com>
2022-06-12 12:01:49 +0200

    added security

---
+++ src/main/java/com/guarenasapp/proyect/security/DatabaseWebSecurity.java
@@ -0,0 +1,25 @@
+package com.guarenasapp.proyect.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, estatus from Usuarios where username=?")
+                .authoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up " +
+                        "inner join Usuarios u on u.id = up.idUsuario " +
+                        "inner join Perfiles p on p.id = up.idPerfil " +
+                        "where u.username = ?");
+    }
+}
